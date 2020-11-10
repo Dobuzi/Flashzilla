2a24918eb61b2529a81b9832c76d6cf0de8b8582
Dobuzi <kimjw7981@gmail.com>
2020-11-10 11:42:52 +0900

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by 김종원 on 2020/11/10.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
